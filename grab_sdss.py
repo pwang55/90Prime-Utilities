152801221e2f3dddd642c2bbb625740a9d96e0ed
Pao-Yu Wang <pwang55@asu.edu>
2020-10-23 17:35:38 -0700

    add photoz and error to grab_sdss

--- grab_sdss.py
+++ grab_sdss.py
@@ -16,16 +16,18 @@ Output four files as ecsv file in data folder:
 
 Stars Query:
     SELECT p.ra, p.dec, p.psfMag_u, p.psfMagErr_u, p.psfMag_g, p.psfMagErr_g, p.psfMag_r, p.psfMagErr_r, p.psfMag_i, p.psfMagErr_i, p.psfMag_z, p.psfMagErr_z, 
-    extinction_u, extinction_g,extinction_r, extinction_i, extinction_z, clean, s.z, s.zerr
+    extinction_u, extinction_g,extinction_r, extinction_i, extinction_z, clean, s.z, s.zerr, pz.z as photoz, pz.zerr as photozerr 
     FROM fGetNearbyObjEq(ra, dec, radius) n, Star p 
-    LEFT OUTER JOIN specObj s ON s.bestObjID=p.objID 
+    LEFT OUTER JOIN specObj s ON s.bestObjID=p.objID
+    LEFT OUTER JOIN photoz pz ON pz.objid=p.objid
     WHERE n.objID=p.objID AND (s.class IS NULL or s.class = "STAR")
 
 Galaxies Query:
     SELECT p.ra, p.dec, p.psfMag_u, p.psfMagErr_u, p.psfMag_g, p.psfMagErr_g, p.psfMag_r, p.psfMagErr_r, p.psfMag_i, p.psfMagErr_i, p.psfMag_z, p.psfMagErr_z, 
-    extinction_u, extinction_g,extinction_r, extinction_i, extinction_z, clean, s.z, s.zerr 
+    extinction_u, extinction_g,extinction_r, extinction_i, extinction_z, clean, s.z, s.zerr, pz.z as photoz, pz.zerr as photozerr 
     FROM fGetNearbyObjEq(ra, dec, radius) n, Galaxy p 
-    LEFT OUTER JOIN specObj s ON s.bestObjID=p.objID 
+    LEFT OUTER JOIN specObj s ON s.bestObjID=p.objID
+    LEFT OUTER JOIN photoz pz ON pz.objid=p.objid 
     WHERE n.objID=p.objID AND (s.class IS NULL or s.class = "GALAXY")
 
 
@@ -75,30 +77,29 @@ else:
 
 # Query for stars with psfMag and extinctions
 q_star = "SELECT p.ra, p.dec, p.psfMag_u, p.psfMagErr_u, p.psfMag_g, p.psfMagErr_g, p.psfMag_r, p.psfMagErr_r, p.psfMag_i, p.psfMagErr_i, p.psfMag_z, p.psfMagErr_z, \
-    extinction_u, extinction_g, extinction_r, extinction_i, extinction_z, clean, s.z, s.zerr \
+    extinction_u, extinction_g, extinction_r, extinction_i, extinction_z, clean, s.z, s.zerr, pz.z as photoz, pz.zerr as photozerr \
     FROM fGetNearbyObjEq("+ra+", "+dec+", "+radius+") n, Star p \
     LEFT OUTER JOIN specObj s ON s.bestObjID = p.objID \
-    WHERE n.objID=p.objID AND (s.class IS NULL or s.class = 'STAR')"
+    LEFT OUTER JOIN photoz pz ON pz.objid = p.objid \
+    WHERE n.objID = p.objID AND(s.class IS NULL or s.class='STAR') "
 
 
 # Query for galaxy with modelMag and cmodelMag and extinctions
 q_gal = "SELECT p.ra, p.dec, p.modelMag_u, p.modelMagErr_u, p.modelMag_g, p.modelMagErr_g, p.modelMag_r, p.modelMagErr_r, p.modelMag_i, p.modelMagErr_i, p.modelMag_z, p.modelMagErr_z, \
     p.cmodelMag_u, p.cmodelMagErr_u, p.cmodelMag_g, p.cmodelMagErr_g, p.cmodelMag_r, p.cmodelMagErr_r, p.cmodelMag_i, p.cmodelMagErr_i, p.cmodelMag_z, p.cmodelMagErr_z, \
-    extinction_u, extinction_g, extinction_r, extinction_i, extinction_z, clean, s.z, s.zerr \
+    extinction_u, extinction_g, extinction_r, extinction_i, extinction_z, clean, s.z, s.zerr, pz.z as photoz, pz.zerr as photozerr \
     FROM fGetNearbyObjEq("+ra+", "+dec+", "+radius+") n, Galaxy p \
     LEFT OUTER JOIN specObj s ON s.bestObjID = p.objID \
-    WHERE n.objID=p.objID AND (s.class IS NULL or s.class = 'GALAXY')"
+    LEFT OUTER JOIN photoz pz ON pz.objid = p.objid \
+    WHERE n.objID = p.objID AND(s.class IS NULL or s.class='GALAXY') "
 
 star_data = CasJobs.executeQuery(q_star, context=context, format='pandas')
 gal_data = CasJobs.executeQuery(q_gal, context=context, format='pandas')
 
 
-
 star_tab = Table(star_data.values, names=star_data.columns, meta={'clustername': clustername, 'ra': ra, 'dec': dec, 'radius': radius}, dtype=[ float for x in range(len(star_data.columns))])
 gal_tab = Table(gal_data.values, names=gal_data.columns, meta={'clustername': clustername, 'ra': ra, 'dec': dec, 'radius': radius}, dtype=[ float for x in range(len(gal_data.columns))])
 
-# star_tab = Table(star_tab, dtype=[float for x in range(len(star_tab.colnames))])
-# gal_tab = Table(star_tab, dtype=[float for x in range(len(gal_tab.colnames))])
 
 snospec = isnan(star_tab['z'])
 gnospec = isnan(gal_tab['z'])
@@ -108,11 +109,20 @@ star_tab['zerr'][snospec] = -1.0
 gal_tab['z'][gnospec] = -1.0
 gal_tab['zerr'][gnospec] = -1.0
 
+snophotoz = isnan(star_tab['photoz'])
+star_tab['photoz'][snophotoz] = -99.0
+star_tab['photozerr'][snophotoz] = -99.0
+
+gnophotoz = gal_tab['photoz'] == -9999
+gal_tab['photoz'][gnophotoz] = -99.0
+gal_tab['photozerr'][gnophotoz] = -99.0
+
+gnanphotoz = isnan(gal_tab['photoz'])
+gal_tab['photoz'][gnanphotoz] = -99.0
+gal_tab['photozerr'][gnanphotoz] = -99.0
+
+
 ascii.write(star_tab, datapath+clustername+'_star_sdss_radec.csv', format='ecsv', overwrite=True)
 ascii.write(gal_tab, datapath+clustername+'_gal_sdss_radec.csv', format='ecsv', overwrite=True)
-
-# ALso save files as csv so that ds9 catalog tools can use them
-# ascii.write(star_tab, datapath+clustername+'_star_sdss_radec.csv', format='csv')
-# ascii.write(gal_tab, datapath+clustername+'_gal_sdss_radec.csv', format='csv')
 
 
