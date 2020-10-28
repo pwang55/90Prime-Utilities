7ea3c7da0b8824e0abe6e96577de827e5bbc0747
Pao-Yu Wang <pwang55@asu.edu>
2020-10-28 02:53:04 -0700

    Update grab_panstarrs and grab_sdss

--- grab_panstarrs.py
+++ grab_panstarrs.py
@@ -13,20 +13,33 @@ Outputs:
 Output files will be saved in data directory.
 
 Query:
-select o.objID, o.raMean, o.decMean, o.nDetections,
-m.gMeanPSFMag, m.gMeanPSFMagErr, m.gMeanKronMag, m.gMeanKronMagErr, m.gQfPerfect, m.rMeanPSFMag, m.rMeanPSFMagErr, m.rMeanKronMag, m.rMeanKronMagErr, m.rQfPerfect,
-m.iMeanPSFMag, m.iMeanPSFMagErr, m.iMeanKronMag, m.iMeanKronMagErr, m.iQfPerfect, m.zMeanPSFMag, m.zMeanPSFMagErr, m.zMeanKronMag, m.zMeanKronMagErr, m.zQfPerfect,
-m.yMeanPSFMag, m.yMeanPSFMagErr, m.yMeanKronMag, m.yMeanKronMagErr, m.yQfPerfect, s.primaryDetection, s.gPSFMag, s.gPSFMagErr, s.gKronMag, s.gKronMagErr, s.rPSFMag,
-s.rPSFMagErr, s.rKronMag, s.rKronMagErr, s.iPSFMag, s.iPSFMagErr, s.iKronMag, s.iKronMagErr, s.zPSFMag, s.zPSFMagErr, s.zKronMag, s.zKronMagErr,
-s.yPSFMag, s.yPSFMagErr,
-s.yKronMag, s.yKronMagErr, s.gpsfMajorFWHM, s.gpsfMinorFWHM, s.gpsfLikelihood, s.gpsfQf, s.gpsfQfPerfect, s.gmomentR1, s.gmomentRH, s.rpsfMajorFWHM, s.rpsfMinorFWHM,
-s.rpsfLikelihood, s.rpsfQf, s.rpsfQfPerfect, s.rmomentR1, s.rmomentRH, s.ipsfMajorFWHM, s.ipsfMinorFWHM, s.ipsfLikelihood, s.ipsfQf, s.ipsfQfPerfect, s.imomentR1,
-s.imomentRH, s.zpsfMajorFWHM, s.zpsfMinorFWHM, s.zpsfLikelihood, s.zpsfQf, s.zpsfQfPerfect, s.zmomentR1, s.zmomentRH,
-s.ypsfMajorFWHM, s.ypsfMinorFWHM, s.ypsfLikelihood, s.ypsfQf, s.ypsfQfPerfect, s.ymomentR1, s.ymomentRH
-from fGetNearbyObjEq(ra,dec,radius) nb
-inner join ObjectThin o on o.objid=nb.objid
-inner join MeanObject m on o.objid=m.objid and o.uniquePspsOBid=m.uniquePspsOBid
-inner join StackObjectView s on o.objid=s.objid and s.primaryDetection=1 and o.uniquePspsOBid=s.uniquePspsOBid
+select o.raMean, o.decMean, o.raStack, o.decStack, o.nDetections,
+m.gMeanPSFMag, m.gMeanPSFMagErr, m.gMeanKronMag, m.gMeanKronMagErr,
+m.rMeanPSFMag, m.rMeanPSFMagErr, m.rMeanKronMag, m.rMeanKronMagErr,
+m.iMeanPSFMag, m.iMeanPSFMagErr, m.iMeanKronMag, m.iMeanKronMagErr,
+m.zMeanPSFMag, m.zMeanPSFMagErr, m.zMeanKronMag, m.zMeanKronMagErr,
+m.yMeanPSFMag, m.yMeanPSFMagErr, m.yMeanKronMag, m.yMeanKronMagErr,
+s.gPSFMag, s.gPSFMagErr, s.gKronMag, s.gKronMagErr, 
+s.rPSFMag, s.rPSFMagErr, s.rKronMag, s.rKronMagErr, 
+s.iPSFMag, s.iPSFMagErr, s.iKronMag, s.iKronMagErr, 
+s.zPSFMag, s.zPSFMagErr, s.zKronMag, s.zKronMagErr,
+s.yPSFMag, s.yPSFMagErr, s.yKronMag, s.yKronMagErr, 
+s.gpsfMajorFWHM, s.gpsfMinorFWHM, s.gpsfLikelihood, 
+s.rpsfMajorFWHM, s.rpsfMinorFWHM, s.rpsfLikelihood, 
+s.ipsfMajorFWHM, s.ipsfMinorFWHM, s.ipsfLikelihood,
+s.zpsfMajorFWHM, s.zpsfMinorFWHM, s.zpsfLikelihood,
+s.ypsfMajorFWHM, s.ypsfMinorFWHM, s.ypsfLikelihood,
+psc.ps_score, o.qualityFlag, o.objinfoFlag, s.primaryDetection, 
+m.gFlags, s.ginfoFlag, s.ginfoFlag2, s.ginfoFlag3,
+m.rFlags, s.rinfoFlag, s.rinfoFlag2, s.rinfoFlag3,
+m.iFlags, s.iinfoFlag, s.iinfoFlag2, s.iinfoFlag3,
+m.zFlags, s.zinfoFlag, s.zinfoFlag2, s.zinfoFlag3,
+m.yFlags, s.yinfoFlag, s.yinfoFlag2, s.yinfoFlag3
+from fGetNearbyObjEq(ra, dec, radius) nb
+join ObjectThin o on o.objid=nb.objid
+join MeanObject m on o.objid=m.objid and o.uniquePspsOBid=m.uniquePspsOBid
+join StackObjectView s on o.objid=s.objid and s.primaryDetection=1 and o.uniquePspsOBid=s.uniquePspsOBid
+join HLSP_PS1_PSC.pointsource_scores psc on psc.objid=o.objid
 
 
 
@@ -72,28 +85,48 @@ else:
 
 
 
-query="""select o.objID, o.raMean, o.decMean, o.nDetections,
-m.gMeanPSFMag, m.gMeanPSFMagErr, m.gMeanKronMag, m.gMeanKronMagErr, m.gQfPerfect, m.rMeanPSFMag, m.rMeanPSFMagErr, m.rMeanKronMag, m.rMeanKronMagErr, m.rQfPerfect,
-m.iMeanPSFMag, m.iMeanPSFMagErr, m.iMeanKronMag, m.iMeanKronMagErr, m.iQfPerfect, m.zMeanPSFMag, m.zMeanPSFMagErr, m.zMeanKronMag, m.zMeanKronMagErr, m.zQfPerfect,
-m.yMeanPSFMag, m.yMeanPSFMagErr, m.yMeanKronMag, m.yMeanKronMagErr, m.yQfPerfect, s.primaryDetection, s.gPSFMag, s.gPSFMagErr, s.gKronMag, s.gKronMagErr, s.rPSFMag,
-s.rPSFMagErr, s.rKronMag, s.rKronMagErr, s.iPSFMag, s.iPSFMagErr, s.iKronMag, s.iKronMagErr, s.zPSFMag, s.zPSFMagErr, s.zKronMag, s.zKronMagErr,
-s.yPSFMag, s.yPSFMagErr,
-s.yKronMag, s.yKronMagErr, s.gpsfMajorFWHM, s.gpsfMinorFWHM, s.gpsfLikelihood, s.gpsfQf, s.gpsfQfPerfect, s.gmomentR1, s.gmomentRH, s.rpsfMajorFWHM, s.rpsfMinorFWHM,
-s.rpsfLikelihood, s.rpsfQf, s.rpsfQfPerfect, s.rmomentR1, s.rmomentRH, s.ipsfMajorFWHM, s.ipsfMinorFWHM, s.ipsfLikelihood, s.ipsfQf, s.ipsfQfPerfect, s.imomentR1,
-s.imomentRH, s.zpsfMajorFWHM, s.zpsfMinorFWHM, s.zpsfLikelihood, s.zpsfQf, s.zpsfQfPerfect, s.zmomentR1, s.zmomentRH,
-s.ypsfMajorFWHM, s.ypsfMinorFWHM, s.ypsfLikelihood, s.ypsfQf, s.ypsfQfPerfect, s.ymomentR1, s.ymomentRH
+query="""select o.raMean, o.decMean, o.nDetections,
+m.gMeanPSFMag, m.gMeanPSFMagErr, m.gMeanKronMag, m.gMeanKronMagErr,
+m.rMeanPSFMag, m.rMeanPSFMagErr, m.rMeanKronMag, m.rMeanKronMagErr,
+m.iMeanPSFMag, m.iMeanPSFMagErr, m.iMeanKronMag, m.iMeanKronMagErr,
+m.zMeanPSFMag, m.zMeanPSFMagErr, m.zMeanKronMag, m.zMeanKronMagErr,
+m.yMeanPSFMag, m.yMeanPSFMagErr, m.yMeanKronMag, m.yMeanKronMagErr,
+s.gPSFMag, s.gPSFMagErr, s.gKronMag, s.gKronMagErr, 
+s.rPSFMag, s.rPSFMagErr, s.rKronMag, s.rKronMagErr, 
+s.iPSFMag, s.iPSFMagErr, s.iKronMag, s.iKronMagErr, 
+s.zPSFMag, s.zPSFMagErr, s.zKronMag, s.zKronMagErr,
+s.yPSFMag, s.yPSFMagErr, s.yKronMag, s.yKronMagErr, 
+s.gpsfMajorFWHM, s.gpsfMinorFWHM, s.gpsfLikelihood, 
+s.rpsfMajorFWHM, s.rpsfMinorFWHM, s.rpsfLikelihood, 
+s.ipsfMajorFWHM, s.ipsfMinorFWHM, s.ipsfLikelihood,
+s.zpsfMajorFWHM, s.zpsfMinorFWHM, s.zpsfLikelihood,
+s.ypsfMajorFWHM, s.ypsfMinorFWHM, s.ypsfLikelihood,
+psc.ps_score, o.qualityFlag, o.objinfoFlag, s.primaryDetection, 
+m.gFlags, s.ginfoFlag, s.ginfoFlag2, s.ginfoFlag3,
+m.rFlags, s.rinfoFlag, s.rinfoFlag2, s.rinfoFlag3,
+m.iFlags, s.iinfoFlag, s.iinfoFlag2, s.iinfoFlag3,
+m.zFlags, s.zinfoFlag, s.zinfoFlag2, s.zinfoFlag3,
+m.yFlags, s.yinfoFlag, s.yinfoFlag2, s.yinfoFlag3
 from fGetNearbyObjEq({},{},{}) nb
-inner join ObjectThin o on o.objid=nb.objid
-inner join MeanObject m on o.objid=m.objid and o.uniquePspsOBid=m.uniquePspsOBid
-inner join StackObjectView s on o.objid=s.objid and s.primaryDetection=1 and o.uniquePspsOBid=s.uniquePspsOBid""".format(ra,dec,radius)
+join ObjectThin o on o.objid=nb.objid
+join MeanObject m on o.objid=m.objid and o.uniquePspsOBid=m.uniquePspsOBid
+join StackObjectView s on o.objid=s.objid and s.primaryDetection=1 and o.uniquePspsOBid=s.uniquePspsOBid
+join HLSP_PS1_PSC.pointsource_scores psc on psc.objid=o.objid""".format(ra,dec,radius)
 
 jobs = mastcasjobs.MastCasJobs(username='pwang55', password='4a552d', context=context)
 results = jobs.quick(query,task_name='python cone search')
 #tab = PC.fixcolnames(PC.ascii.read(results))
 
-results_tab = Table(results, meta={'ra': ra,'dec': dec,'radius': radius})
+results_tab = Table(results, meta={'clustername': clustername, 'ra': ra,'dec': dec,'radius': radius})
 
-ascii.write(results_tab, datapath+clustername+'_panstarrs_radec.csv',format='ecsv')
+# # Convert all -999 entry to -99 for easier table handling in other scripts
+# for c in range(len(results_tab.colnames)):
+#     colname = results_tab.colnames[c]
+#     h = results_tab[colname] == -999
+#     results_tab[colname][h] = -99.0
+
+
+ascii.write(results_tab, datapath+clustername+'_panstarrs_radec.csv',format='ecsv', overwrite=True)
 
 
 
